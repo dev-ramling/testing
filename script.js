03d6231ab2b305a66d483ad33549b84528cee4e8
RamDev <ramling.dev@gmail.com>
2024-06-03 15:07:10 +0530

    Added fun to find longest word in given string

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 const a = 'welcome to the jungle'
 
-// Path: script.js
+// function to find longest word in a string
+function findLongestWord(str) {
+  const words = str.split(' ')
+  let longestWord = ''
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > longestWord.length) {
+      longestWord = words[i]
+    }
+  }
+  return longestWord
+}
+
+// export the function
+module.exports = findLongestWord
